cacbfde83d738957bdea4707617896c4e956c8df
staran <staran@STARAN-NB>
2021-12-13 19:36:50 +0500

    Commit1

---
+++ TestLesson0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SomeLib;
+
+namespace TestLesson0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 20;
+            int y = 10;
+
+            Console.WriteLine("x + y = {0}", Calc.Sum(x,y));
+
+            Console.WriteLine("Hello World! - Study1");
+
+       
+        }
+    }
+}
